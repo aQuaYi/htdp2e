92cf268302748b089be38d71226b463019af000a
aQua <6028869+aQuaYi@users.noreply.github.com>
2022-01-13 15:25:28 +0800

    添加了第一段代码

---
+++ 2 - 开篇：如何编程/1.rkt
@@ -0,0 +1,48 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname |1|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+(+ 1 1)
+
+(+ 2 2)
+
+(* 3 3)
+
+(- 4 2)
+
+(/ 6 2)
+
+;;;;
+
+(sqr 3)
+
+(expt 2 3)
+
+(sin 0)
+
+(cos pi)
+;; #i 表示答案并不精确
+
+;;;;
+
+( + 2 (+ 3 4))
+
+(+ 2 3 4)
+
+;;;;
+
+(+ 2 (+ (* 3 3) 4))
+
+(+ 2 (+ (* 3 (/ 12 4)) 4))
+
+(+ (* 5 5) (+ (* 3 (/ 12 4)) 4))
+
+;;;;
+
+; (+ (1) (2))
+
+;;;;
+
+(+ 1 2 3 4 5 6 7 8 9 0)
+
+(* 1 2 3 4 5 6 7 8 9 0)
+
