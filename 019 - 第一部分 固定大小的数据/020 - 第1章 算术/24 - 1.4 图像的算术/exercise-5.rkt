2c05224263e2dcb4a42275e07e0bc5eb4b2b89ff
aQua <6028869+aQuaYi@users.noreply.github.com>
2022-02-17 11:58:56 +0800

    Create exercise-5.rkt

---
+++ 019 - 第一部分 固定大小的数据/020 - 第1章 算术/24 - 1.4 图像的算术/exercise-5.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname exercise-5) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+(place-image
+    (isosceles-triangle 60 30 "solid" "green")
+    50 30
+    (place-image (rectangle 10 40 "solid" "gray")
+                 50 77
+                 (empty-scene 100 100)))
+ 
